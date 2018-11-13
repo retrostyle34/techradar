fcfb51599d3b4bed66256f66b5a0455d9b244111
Lubos Sivecky <lubo.sivecky@hotmail.com>
2018-11-14 00:12:24 +0100

    Implemented new REST API features
    Implemented methods - replaceItem, deleteItem and saveItem

--- src/main/java/eu/ubi/techradar/controller/WebController.java
+++ src/main/java/eu/ubi/techradar/controller/WebController.java
@@ -5,9 +5,7 @@ import eu.ubi.techradar.entity.Item;
 import eu.ubi.techradar.exceptions.ItemNotFoundException;
 import eu.ubi.techradar.repository.ItemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -16,21 +14,45 @@ import java.util.Optional;
 public class WebController {
 
     @Autowired
-    private ItemRepository itemRepository;
+    private ItemRepository repository;
 
     @GetMapping("/items")
     public List<Item> retrieveAllItems() {
-        return itemRepository.findAll();
+        return repository.findAll();
     }
 
-    @GetMapping("/item/{id}")
-    public Item retrieveItem(@PathVariable long id) {
-        Optional<Item> item = itemRepository.findById(id);
+    @PostMapping("/items")
+    public Item saveItem(@RequestBody Item item) {
+        return repository.save(item);
+    }
 
-        if (!item.isPresent())
-            throw new ItemNotFoundException("id: " + id);
 
-        return item.get();
+    @GetMapping("/items/{id}")
+    public Item retrieveItem(@PathVariable Long id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new ItemNotFoundException("id: "+id));
+    }
+
+    @PostMapping("/items/{id}")
+    public Item replaceItem(@RequestBody Item newItem, @PathVariable Long id) {
+        return repository.findById(id)
+                .map(item -> {
+                    item.setName(newItem.getName());
+                    item.setDetails(newItem.getDetails());
+                    item.setLevel(newItem.getLevel());
+                    item.setType(newItem.getType());
+                    item.setChangeDate(newItem.getChangeDate());
+                    return repository.save(newItem);
+                })
+                .orElseGet(() -> {
+                    newItem.setId(id);
+                    return repository.save(newItem);
+                });
+    }
+
+    @DeleteMapping("/items/{id}")
+    public void deleteItem(@PathVariable Long id) {
+        repository.deleteById(id);
     }
 
 }
